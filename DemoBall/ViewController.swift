c39866b7833d4d63ee0c93efa8a8f384ce99cf9b
ttb10912 <ttb10912@TTB10912s-MacBook-Pro.local>
2016-09-15 14:43:30 +0700

    .
    .

--- DemoBall/ViewController.swift
+++ DemoBall/ViewController.swift
@@ -9,15 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var ball = UIImageView()
+    var radians = CGFloat()
+    var ballRadious = CGFloat()
+    var toTheEnd:Bool = false
+    var reverseRadians = CGFloat()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addBall()
+        let timer = NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector: #selector(rollingBall), userInfo: nil, repeats: true)
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func addBall() {
+        
+        let mainViewSize = self.view.bounds.size
+        ball = UIImageView(image: UIImage(named: "ball.png"))
+        ballRadious = 32.0
+        ball.center = CGPointMake(ballRadious, mainViewSize.height/2 )
+        self.view.addSubview(ball)
+        
+        
+        
+    }
+    
+    func rollingBall()  {
+        
+        var deltaAngle : CGFloat = 0.1
+        radians = radians + deltaAngle
+        reverseRadians = reverseRadians - deltaAngle
+        
+        
+        print(self.ball.center.x)
+        print(self.view.bounds.size.width - 32)
+        
+        
+        if toTheEnd == false {
+            
+            
+            if (self.ball.center.x  >=  28.0 ) {
+                //bong lan
+                self.ball.transform = CGAffineTransformMakeRotation(self.radians)
+                
+                //bong lan sang phai
+                self.ball.center = CGPointMake(self.ball.center.x + self.ballRadious * deltaAngle , self.ball.center.y )
+                
+                if self.ball.center.x  >= self.view.bounds.size.width - 32 {
+                    toTheEnd = true
+                    
+                }
+            }
+            
+           
+            
+        }
+        if toTheEnd == true {
+            
+            if ( self.ball.center.x  <= self.view.bounds.size.width - 30) {
+                
+                //bong lan NGUOC
+                self.ball.transform = CGAffineTransformMakeRotation(self.reverseRadians)
+                
+                //lan qua trai
+                self.ball.center = CGPointMake(self.ball.center.x  - self.ballRadious * deltaAngle , self.ball.center.y )
+                
+                if self.ball.center.x  <=  32 {
+                    toTheEnd = false
+                }
+            }
+           
+            
+        }
     }
 
 
